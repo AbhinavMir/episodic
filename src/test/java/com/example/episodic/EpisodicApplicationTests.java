0be4499649702b549fd7175ee12d0e73be0d2cc9
Abhinav Srivastava <atg271@gmail.com>
2024-01-30 04:56:18 -0500

    wip

---
+++ src/test/java/com/example/episodic/EpisodicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.episodic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpisodicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
