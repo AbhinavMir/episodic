60105c59680f18d8944c3f557e46502e659d1605
Abhinav Srivastava <atg271@gmail.com>
2024-01-30 16:39:17 -0500

    wip

--- src/main/java/com/example/episodic/model/User.java
+++ src/main/java/com/example/episodic/model/User.java
@@ -7,11 +7,45 @@ import javax.persistence.Id;
 
 @Entity
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String email;
     private String password;
-    private String username;
-    // Getters and setters
+
+    // Constructors
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
